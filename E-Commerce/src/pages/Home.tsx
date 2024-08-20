520e41d01dff4b0a975db59c575c9c85e4e9a2ce
cynthia-isutsa <isutsacynthia@gmail.com>
2024-08-20 06:00:48 +0300

    chages

--- E-Commerce/src/pages/Home.tsx
+++ E-Commerce/src/pages/Home.tsx
@@ -1,8 +1,25 @@
+import { Categories } from "../assets/MorkData"
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>SHOP BY CATEGORIES</div>
+          </div>
+          <ul>
+            {Categories?.map((item: any, index: any) =>(
+              <li key={index}>{item}</li>
+            ) )}
+          </ul>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
